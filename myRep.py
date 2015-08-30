6c2409b3f6d2b1cc37a2415dd731a26a46210992
Abhinav Chourasia <abhinav.chourasia@olacabs.com>
2015-08-30 17:23:33 +0530

    Added support for modifying headers and path of a request. You can add a new header, make changes to an existing one etc. Simillarly you can do the same with the URL path.

--- myRep.py
+++ myRep.py
@@ -130,7 +130,7 @@ class RequestLogger(controller.Master):
 				requestPort : serializeThisRequest.port,
 				requestScheme : serializeThisRequest.scheme,
 				requestMethod : serializeThisRequest.method,
-				requestPath : serializeThisRequest.path,
+				requestPath : serializeThisRequest.get_path_components(),
 				requestHeaders : serializeThisRequest.headers,
 				requestQueryParams : serializeThisRequest.get_query(),
 				requestBody : serializeThisRequest.get_decoded_content()
@@ -228,16 +228,19 @@ class RepeaterModule(RequestLogger):
 	# ----------------------------------------------------------------------------------------------------------------------------------------
 	def requestMaker(self, requestNumber):
 		print "\n Preparing the request object in requestMaker !"
-		friend = {}
+		totalNoOfParams = 0
+		reqCompoContainer = {}
+		userEditContainer = {}
+		myDic = {}
 		try:
 			myFile = open(self.rawRequestFile, "rb")
 			print "\nFile opened !"
 			while 1:
 				try:
-					friend.update(pickle.load(myFile))
+					reqCompoContainer.update(pickle.load(myFile))
 				except EOFError:
 					break # no more data in the file
-			print friend
+			print reqCompoContainer
 
 			portKey = "port # " + str(requestNumber)
 			schemeKey = "scheme # " + str(requestNumber)
@@ -246,26 +249,57 @@ class RepeaterModule(RequestLogger):
 			headersKey = "headers # " + str(requestNumber)
 			query_paramsKey = "query_params # " + str(requestNumber)
 			request_bodyKey = "request_body # " + str(requestNumber)
-		
-			print "\n\nYour chosen request has the following parameters : "
-			print "\nPort :" + str(friend.get(portKey))
-			print "\nScheme :" + str(friend.get(schemeKey))
-			print "\nMethod :" + str(friend.get(methodKey))
-			print "\nPath :" + str(friend.get(pathKey))
-			print "\nHeaders :" + str(friend.get(headersKey))
-			print "\nQuery Parameters :" + str(friend.get(query_paramsKey))
-			print "\nRequest Body :" + str(friend.get(request_bodyKey))
+
+			myDic = reqCompoContainer.get(headersKey)
+						
+			# dictionary containing data of request chosen by user
+			userEditContainer.update({
+				'requestPort' : reqCompoContainer.get(portKey),
+				'requestScheme' : reqCompoContainer.get(schemeKey),
+				'requestMethod' : reqCompoContainer.get(methodKey),
+				'requestPath' : reqCompoContainer.get(pathKey),
+				'requestHeaders' : myDic,
+				'requestQueryParams' : reqCompoContainer.get(query_paramsKey),
+				'requestBody' : reqCompoContainer.get(request_bodyKey)
+				})
+
+			print "\n\nYour chosen request has the following components : "
+			print "\n1. Port :" + str(reqCompoContainer.get(portKey))
+			print "\n2. Scheme :" + str(reqCompoContainer.get(schemeKey))
+			print "\n3. Method :" + str(reqCompoContainer.get(methodKey))
+			print "\n4. Path :" + str(reqCompoContainer.get(pathKey))
+			print "\n5. Headers :" + str(reqCompoContainer.get(headersKey))
+			print "\n6. Query Parameters :" + str(reqCompoContainer.get(query_paramsKey))
+			print "\n7. Request Body :" + str(reqCompoContainer.get(request_bodyKey))
+
+			print "\n\nTamper components ? (Y/N)\nY - Yes, tamper it !\nN - No, Just repeat the selected request"
+			choice = raw_input("\nEnter Y/N (any other character leads to termination of the script): ")
 			
+			if (choice.lower() == 'y' ):
+				self.tamperData(userEditContainer)
+			else: 
+				if (choice.lower() == 'n'):
+					print "\n\nCalling the request send function ....just repeating the selected request without any tampering"
+					self.sendRequest()
+				else:
+					print "\nInvalid choice !\n\n"
+					exit(1)
+
+
 			if not myFile.closed:
 				myFile.close()
 
 			print "\n Want to replay another request ?"
-			choice = raw_input("\nEnter Y/N (any other charater leads to termination of the script): ")
+			choice = raw_input("\nEnter Y/N (any other character leads to termination of the script): ")
 			if (choice.lower() == 'y' ):
 				self.chooseRequest()
-			else: 
-				print "\nThanks for using the script... more functionality enroute !"
-				exit(1)
+			else:
+				if(choice.lower() == 'n' ): 
+					print "\nThanks for using the script... more functionality enroute !"
+					exit(1)
+				else:
+					print "\nInvalid choice !\n\n"
+					exit(1)
 
 		except IOError:
 			print "\n\nThere was an error r/w -ing the file !! \n\n"
@@ -273,6 +307,171 @@ class RepeaterModule(RequestLogger):
 			myFile.close()
 			if (myFile.closed):
 				print "\nFile is now closed !"
+	# ----------------------------------------------------------------------------------------------------------------------------------------
+
+	
+	# ----------------------------------------------------------------------------------------------------------------------------------------
+	def tamperData(self, dataToTamperContainer):
+		print "\n\nTampering data now !"
+		print dataToTamperContainer
+		componentSize = 0
+		genericCounter = 0
+		newKey = ""
+		newValue = ""
+
+		choice = input("\nEnter the component number to tamper (1-7) ")
+		
+		if (choice >= 1 and choice <= 7):
+			
+			# handling path component
+			if (choice == 4):
+				print "\n\nPath component"
+				componentSize = len(dataToTamperContainer.get('requestPath'))
+				
+				print "\n\nParameters of this component are \n"
+				while (genericCounter < componentSize):
+					print str(genericCounter+1) + "." + str(dataToTamperContainer.get('requestPath')[genericCounter])
+					genericCounter = genericCounter + 1 
+				
+				print "\n\nAdd additional parameters here ?\nY - Yes, add !\nN - No, tamper existing"
+				choice = raw_input("\nEnter Y/N (any other character leads to termination of the script): ")
+				
+				if (choice.lower() == 'y' ):
+					while 1:
+						print "\nEnter position to insert (Valid is 1 - " + str(componentSize) + ")"
+						choice = input()
+						
+						if(choice >=1 and choice <= componentSize):
+							print "\nEnter the parameter you want to add \n"
+							newValue = raw_input()
+							dataToTamperContainer.get('requestPath').insert(choice - 1, newValue)
+							print "\nRequest updated"
+						else:
+							print "\n\nInvalid choice !"
+							exit(1)
+						componentSize = len(dataToTamperContainer.get('requestPath'))
+						print "\n\nAdd more (y/n)"
+						addMore = raw_input()
+						if(addMore.lower() == 'y'):
+							continue
+						else:
+							if(addMore.lower() == 'n'):
+								break
+							else:
+								print "\n\nInvalid choice !"
+								exit(1)
+				else:
+					if(choice.lower() == 'n'):
+						print "\nEnter parameter number to tamper (Valid is 1 - " + str(componentSize) + ")"
+						choice = input()
+						if(choice >=1 and choice <= componentSize):
+							print "\n\nEnter the new value"
+							newValue = raw_input()
+							dataToTamperContainer.get('requestPath')[choice] = newValue
+							print "\nRequest updated"
+						else:
+							print "\n\nInvalid choice !"
+							exit(1)
+					else:
+						print "\n\nInvalid choice !"
+						exit(1)
+
+				genericCounter = 0
+				print "\n\nParameters of this component NOW are \n"
+				while (genericCounter < componentSize):
+					print str(genericCounter+1) + "." + str(dataToTamperContainer.get('requestPath')[genericCounter])
+					genericCounter = genericCounter + 1 
+
+			# handling headers
+			if(choice == 5):
+				print "\n\nHeader component"
+				genericCounter = 0
+				componentSize = len(dataToTamperContainer.get('requestHeaders'))
+				print "\n\nParameters of this component are \n"
+				while (genericCounter < componentSize):
+					#print "\n\nInside while"
+					print str(genericCounter+1) + "." + str(dataToTamperContainer.get('requestHeaders').keys()[genericCounter]) + ": " + str(dataToTamperContainer.get('requestHeaders')[dataToTamperContainer.get('requestHeaders').keys()[genericCounter]]) 
+					genericCounter = genericCounter + 1 
+
+				'''for headerName, headerValue in dataToTamperContainer['requestHeaders']:
+					print str(genericCounter+1) + ". " + headerName + ": " + headerValue
+					genericCounter = genericCounter + 1'''
+
+				print "\n\nAdd additional headers here ?\nY - Yes, add !\nN - No, tamper existing"
+				choice = raw_input("\nEnter Y/N (any other character leads to termination of the script): ")
+				
+				'''print "\nblalalalallaklajskdjsldkjsldksldkslkd" 
+				print dataToTamperContainer.get('requestHeaders')
+				print type(dataToTamperContainer.get('requestHeaders'))
+				print dataToTamperContainer.get('requestHeaders').keys()
+				print dataToTamperContainer.get('requestHeaders').keys()
+				print dataToTamperContainer.get('requestHeaders').keys()'''
+
+				if (choice.lower() == 'y' ):
+					while 1:
+						newKey = raw_input("\nKey : ")
+						newValue = raw_input("\nValue :")
+						dataToTamperContainer.get('requestHeaders')[newKey] = [newValue]
+						componentSize = len(dataToTamperContainer.get('requestHeaders'))
+						print "\n\nRequest updated"
+						print "\n\nAdd more (y/n)"
+						addMore = raw_input()
+						if(addMore.lower() == 'y'):
+							continue
+						else:
+							if(addMore.lower() == 'n'):
+								break
+							else:
+								print "\n\nInvalid choice !"
+								exit(1)
+					print "\n\nThe updated headers are \n"
+					genericCounter = 0
+					for headerName, headerValue in dataToTamperContainer['requestHeaders']:
+						print str(genericCounter+1) + ". " + headerName + ": " + headerValue
+						genericCounter = genericCounter + 1
+
+				else:
+					if(choice.lower() == 'n'):
+						print "\nEnter parameter number to tamper (Valid is 1 - " + str(componentSize) + ")"
+						choice = input()
+						
+						if(choice >=1 and choice <= componentSize):
+							print "\nNew Key :"
+							newKey = raw_input()
+							print "\nNew Value :"
+							newValue = raw_input()
+							print "\n\nthe selected key is "
+							print dataToTamperContainer.get('requestHeaders').keys()[choice - 1]
+							del dataToTamperContainer.get('requestHeaders')[dataToTamperContainer.get('requestHeaders').keys()[choice - 1]]
+							dataToTamperContainer.get('requestHeaders')[newKey] = [newValue]
+							componentSize = len(dataToTamperContainer.get('requestHeaders'))
+							print "\n\nRequest updated"
+						
+						else:
+							print "\n\nInvalid choice !"
+							exit(1)
+
+						print "\n\nThe updated headers are \n"
+						genericCounter = 0
+						for headerName, headerValue in dataToTamperContainer['requestHeaders']:
+							print str(genericCounter+1) + ". " + headerName + ": " + headerValue
+							genericCounter = genericCounter + 1
+
+					else:
+						print "\n\nInvalid choice !"
+						exit(1)
+
+		else:
+			print "\n\nInvalid choice !"
+			exit(1)
+
+		self.sendRequest()
+	# ----------------------------------------------------------------------------------------------------------------------------------------
+
+	
+	# ----------------------------------------------------------------------------------------------------------------------------------------
+	def sendRequest(self):
+		print "\n\nSending the request now ...! "
 	# ----------------------------------------------------------------------------------------------------------------------------------------
 ##############################################################################################################################################
 
